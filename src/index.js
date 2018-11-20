aa3f25e7a5b761db43688d29149a1010c7160d21
Carlos Rodriguez <carlos.rodar7@gmail.com>
2018-11-20 10:39:46 -0600

    App Completed

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import App from './components/App';
+import FavoriteRecipeList from './components/FavoriteRecipeList';
+import './styles/index.css';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer);
+store.subscribe(() => console.log('store',store.getState()));
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch> 
+                <Route exact path='/' component={App}/>
+
+                <Route path='/favorites' component={FavoriteRecipeList}/>
+            </Switch>
+        </BrowserRouter>
+        
+    </Provider>, document.getElementById('root')
+);
+
+
